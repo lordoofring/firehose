bde043caf180505d34e0f3e5c8dc546018ca50bd
vboxuser <bsyoussef4@gmail.com>
2025-09-26 00:42:51 -0600

    sb3_contrib instead of sb3, call for Maskable PPO + gymnasium3 comabtible

--- cell2fire/visualize_experiment.py
+++ cell2fire/visualize_experiment.py
@@ -1,36 +1,41 @@
 import logging
 import time
-
+import os
 from gymnasium.wrappers import RecordVideo
-from stable_baselines3 import PPO
+#from stable_baselines3 import Maskab
+from sb3_contrib import MaskablePPO
 
-from stable_baselines3 import PPO
+#from stable_baselines3 import PPO
 
 from gym_env import FireEnv
 from firehose.helpers import IgnitionPoints, IgnitionPoint
-
+pre_train_dir = './pretrained_models'
+model_path_list = os.listdir(pre_train_dir)
+# index3: 40x40 fixed_ppo, index4: 40x40 random_ppo (all maskable)
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    model = PPO.load("../vectorize_model_2022-04-11_22-36-35/ppo_final.zip")
-    eval_env = FireEnv(ignition_points=IgnitionPoints([IgnitionPoint(1100, 1)]))
-    obs = eval_env.reset()
-    video_recorder = RecordVideo(eval_env, "ppo_vid.mp4", enabled=True)
-
-    for i in range(1000):
+    model = MaskablePPO.load(os.path.join(pre_train_dir,model_path_list[3]))
+    eval_env = FireEnv(ignition_points=IgnitionPoints([IgnitionPoint(1100, 1)]),observation_type='forest_rgb')
+    obs , info = eval_env.reset()
+    eval_env = RecordVideo(eval_env, 
+                           video_folder="videos",
+                           name_prefix="Maskable_init",
+                           episode_trigger=lambda x : x % 10 == 0)
+    eval_env.start_recording('40x40_test_Masked_PPO')
+    for i in range(500):
         action, _states = model.predict(obs, deterministic=True)
-        obs, reward, done, info = eval_env.step(action)
-
+        obs, reward, truncated,terminated, info = eval_env.step(action)
         eval_env.render()
-        video_recorder.capture_frame()
 
         print("\n", action, reward)
         time.sleep(0.025)
-        if done:
-            obs = eval_env.reset()
+        if terminated or truncated:
+            obs, info = eval_env.reset()
             break
+        
+            
 
-    video_recorder.close()
-    video_recorder.enabled = False
+    eval_env.stop_recording()
     eval_env.close()
 
